219916c74ae93db289af3e659b0cca57b65e131c
Claire Liang <caliang@usc.edu>
2022-11-20 22:27:32 -0800

    position = 1

--- Tommy Arcade/src/main/java/game/CardDeck.java
+++ Tommy Arcade/src/main/java/game/CardDeck.java
@@ -7,7 +7,7 @@ public class CardDeck {
 	private int position;
 	
 	public CardDeck() {
-		int position = 0;
+		int position = 1;
 		String[] temp = {
 				"2-S", "3-S", "4-S", "5-S", "6-S", "7-S", "8-S", "9-S", "10-S", "J-S", "Q-S", "K-S", "A-S"
 		};
