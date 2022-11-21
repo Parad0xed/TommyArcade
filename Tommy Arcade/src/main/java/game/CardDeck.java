1455442d5bd6ffed9b0b10c6adebb45461789f13
Parad0xed <etzhong@usc.edu>
2022-11-20 22:17:18 -0800

    Test Commit

--- Tommy Arcade/src/main/java/game/CardDeck.java
+++ Tommy Arcade/src/main/java/game/CardDeck.java
@@ -6,6 +6,14 @@ public class CardDeck {
 	private Vector<String> deck;
 	private int position;
 	
+	public CardDeck() {
+		int position = 0;
+		String[] temp = {
+				"2-S", "3-S", "4-S", "5-S", "6-S", "7-S", "8-S", "9-S", "10-S", "J-S", "Q-S", "K-S", "A-S"
+		};
+		deck = new Vector<String>();
+		
+	}
 	
 	
 }
