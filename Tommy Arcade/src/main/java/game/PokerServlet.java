52cbb2e3551d0d164be210a6660c81b3800f1f74
Parad0xed <etzhong@usc.edu>
2022-11-30 07:42:06 -0800

    Partial Poker Implementation

--- Tommy Arcade/src/main/java/game/PokerServlet.java
+++ Tommy Arcade/src/main/java/game/PokerServlet.java
@@ -9,24 +9,89 @@ import java.text.DecimalFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Vector;
+import java.util.Formatter.BigDecimalLayoutForm;
 
 import javax.*;
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
-import org.json.*;
+
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.apache.commons.*;
+import org.json.simple.parser.ParseException;
 
 @WebServlet("/PokerServlet")
 public class PokerServlet extends HttpServlet{
 	
-private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 	
 	protected void service(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
+		String action = request.getParameter("action");
+		String username = request.getParameter("username");
 		
+		if(action.equals("INIT")) {
+			
+			//Get draws for given game
+			Vector<String> draws = new Vector<String>();
+			CardDeck deck = new CardDeck();
+			for(int i=0; i<9; i++) {
+				draws.add(deck.getNextCard());
+			}
+			
+			//Call Poker API
+			String temp = "https://api.pokerapi.dev/v1/winner/texas_holdem?cc=";
+			for(int i=4; i<9; i++) {
+				temp += draws.get(i);
+				if(i < 8) temp += ",";
+			}
+			temp += ("&pc[]=" + draws.get(0) + "," + draws.get(1));
+			temp += ("&pc[]=" + draws.get(2) + "," + draws.get(3));
+			
+			
+			URL url = new URL(temp);
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			JSONObject data = null;
+			
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer content = new StringBuffer();
+			while((inputLine = in.readLine()) != null) {
+				content.append(inputLine);
+			}
+			in.close();
+			try {
+				data = (JSONObject)(new JSONParser().parse(content.toString()));
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			
+			JSONArray buffer = (JSONArray) data.get("winners");
+			JSONObject winData = (JSONObject) buffer.get(0);
+			String winningHand = winData.get("result").toString();
+			String winner = winData.get("cards").toString().substring(0, 2);
+			if(winner.equals(draws.get(0))) {//Player Wins
+				winner = "player";
+			}
+			else {//Opponent Wins
+				winner = "opponent";
+			}
+			if(buffer.size() > 1) winner = "tie";
+			
+			//Write Response JSON
+			PrintWriter out = response.getWriter();
+			response.setContentType("application/json");
+			out.println("{");
+			out.println("\"player\":[\"" + draws.get(0) + "\", \"" + draws.get(1) + "\"],");
+			out.println("\"opponent\":[\"" + draws.get(2) + "\", \"" + draws.get(3) + "\"],");
+			out.println("\"community\":[\"" + draws.get(4) + "\", \"" + draws.get(5) + "\", \"" 
+			+ draws.get(6) + "\", \"" + draws.get(7) + "\", \"" + draws.get(8) + "\"],");
+			out.println("\"winningHand\": \"" + winningHand + "\",");
+			out.println("\"winner\": \"" + winner + "\"");
+			out.println("}");
+		}
 		
 		
 	}
--- Tommy Arcade/src/main/webapp/poker.js
+++ Tommy Arcade/src/main/webapp/poker.js
@@ -1,23 +1,50 @@
+var playerCards = [];
+var opponentCards = [];
+var communityCards = [];
+var odds = [];
+var playerBet = 0;
+var opponentBet = 0;
+var pot = 0;
+
+function startGame(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        console.log(this.responseText);
+        data = JSON.parse(this.responseText);
+        playerCards = data["player"];
+        opponentCards = data["opponent"];
+        communityCards = data["community"];
+
+        document.getElementById("playerCard1").src = "images/cards/" + playerCards[0] + ".png";
+        document.getElementById("playerCard2").src = "images/cards/" + playerCards[1] + ".png";
+    };
+    // /username = getCookie("username");
+    username = "Player1"
+    xhttp.open("GET", "/Tommy_Arcade/PokerServlet" + "?username=" + username + "&action=" + "INIT", true);
+    xhttp.send();
+}
+
+function resetGame(){
+    playerCards = [];
+    opponentCards = [];
+    commuintyCards = [];
+}
+
 function flop(){
-    console.log("Flopping the Cards")
-    document.getElementById("flop1").src = "images/cards/AS.png";
-    document.getElementById("flop2").src = "images/cards/AC.png";
-    document.getElementById("flop3").src = "images/cards/AH.png";
+    document.getElementById("flop1").src = "images/cards/" + communityCards[0] + ".png";
+    document.getElementById("flop2").src = "images/cards/" + communityCards[1] + ".png";
+    document.getElementById("flop3").src = "images/cards/" + communityCards[2] + ".png";
 }
 
 function turn(){
-    document.getElementById("turn").src = "images/cards/10D.png";
+    document.getElementById("turn").src = "images/cards/" + communityCards[3] + ".png";
 }
 
 function river(){
-    document.getElementById("river").src = "images/cards/KC.png"
+    document.getElementById("river").src = "images/cards/" + communityCards[4] + ".png";
 }
 
-function playerShow(){
-    document.getElementById("playerCard1").src = "images/cards/8C"
-    document.getElementById("playerCard2").src = "images/cards/8S"
-}
-
-function opponentShow(){
-
+function showdown(){
+    document.getElementById("opponentCard1").src = "images/cards/" + opponentCards[0] + ".png";
+    document.getElementById("opponentCard2").src = "images/cards/" + opponentCards[1] + ".png";
 }
