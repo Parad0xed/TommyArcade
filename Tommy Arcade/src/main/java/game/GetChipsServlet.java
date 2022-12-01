542ad7911255e5bff32638b2b1c4212022d1c2e6
Parad0xed <etzhong@usc.edu>
2022-11-30 22:56:41 -0800

    Added GetChipsServlet

---
+++ Tommy Arcade/src/main/java/game/GetChipsServlet.java
@@ -0,0 +1,27 @@
+package game;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import login_signup.JDBCConnector;
+
+@WebServlet("/GetChipServlet")
+public class GetChipServlet extends HttpServlet{
+private static final long serialVersionUID = 1L;
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		String username = request.getParameter("username");
+		response.setContentType("text/plain");
+		JDBCConnector.getChips(username);
+		PrintWriter out = response.getWriter();
+		out.println(JDBCConnector.getChips(username));
+		out.close();
+	}
+}
