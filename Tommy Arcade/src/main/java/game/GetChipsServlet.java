b83b508cc1a53437874c0c33ef13691c9a377d02
Parad0xed <etzhong@usc.edu>
2022-11-30 22:57:11 -0800

    Implemented GetChipsServlet

--- Tommy Arcade/src/main/java/game/GetChipsServlet.java
+++ Tommy Arcade/src/main/java/game/GetChipsServlet.java
@@ -11,8 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import login_signup.JDBCConnector;
 
-@WebServlet("/GetChipServlet")
-public class GetChipServlet extends HttpServlet{
+@WebServlet("/GetChipsServlet")
+public class GetChipsServlet extends HttpServlet{
 private static final long serialVersionUID = 1L;
 	
 	protected void service(HttpServletRequest request, HttpServletResponse response) 
